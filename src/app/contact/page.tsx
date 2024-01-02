d449b67565c71cb58c27651a40a7a4684b041ad2
darkxweb <khijendradighore19357@gmail.com>
2024-01-02 14:46:08 +0530

    Contact Form Response Functionality Added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -41,11 +41,16 @@ const Contact: NextPage = () => {
           </div>
           {/* Contact Form */}
           <div className="row">
-            <form className="contact-form padd-15">
+            <form
+              className="contact-form padd-15"
+              action="https://formspree.io/f/xayryovq"
+              method="POST"
+            >
               <div className="row">
                 <div className="form-item col-6 padd-15">
                   <div className="form-group">
                     <input
+                      name="fname"
                       type="text"
                       className="form-control"
                       placeholder="Name*"
@@ -55,6 +60,7 @@ const Contact: NextPage = () => {
                 <div className="form-item col-6 padd-15">
                   <div className="form-group">
                     <input
+                      name="email"
                       type="email"
                       className="form-control"
                       placeholder="Email*"
@@ -66,6 +72,7 @@ const Contact: NextPage = () => {
                 <div className="form-item col-12 padd-15">
                   <div className="form-group">
                     <input
+                      name="subject"
                       type="text"
                       className="form-control"
                       placeholder="Subject*"
@@ -77,6 +84,7 @@ const Contact: NextPage = () => {
                 <div className="form-item col-12 padd-15">
                   <div className="form-group">
                     <textarea
+                      name="message"
                       className="form-control"
                       placeholder="Your Message*"
                       defaultValue={""}
